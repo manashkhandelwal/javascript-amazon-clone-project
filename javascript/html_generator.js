1ba7f47c7ffb21a7cf7a3aa92d8d7c60dc3986dc
manashkhandelwal <manashkhandelwal171@gmail.com>
2024-01-11 19:51:51 +0545

    Use product.js file

--- javascript/html_generator.js
+++ javascript/html_generator.js
@@ -1,28 +1,3 @@
-products=[{
-    img:"images/products/athletic-cotton-socks-6-pairs.jpg",
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    ratings:{
-        stars:4.5,
-        count:87
-    },
-    priceCents:1090
-}, {
-    img:"images/products/intermediate-composite-basketball.jpg",
-    name:' Intermediate Size Basketball',
-    ratings:{
-        stars:4,
-        count:127
-    },
-    priceCents:2095
-},  {
-    img:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name:' Adults Plain Cotton T-Shirt - 2 Pack',
-    ratings:{
-        stars:4.5,
-        count:56
-    },
-    priceCents:799
-}];
 let productsHTML='';
 
 products.forEach((product) => {
@@ -30,7 +5,7 @@ products.forEach((product) => {
         <div class="product-container">
             <div class="product-image-container">
                 <img class="product-image"
-                src="${product.img}">
+                src="${product.image}">
             </div>
 
             <div class="product-name limit-text-to-2-lines">
@@ -39,9 +14,9 @@ products.forEach((product) => {
 
             <div class="product-rating-container">
                 <img class="product-rating-stars"
-                src="images/ratings/rating-${product.ratings.stars *10}.png">
+                src="images/ratings/rating-${product.rating.stars *10}.png">
                 <div class="product-rating-count link-primary">
-                    ${product.ratings.count}
+                    ${product.rating.count}
                 </div>
             </div>
 
