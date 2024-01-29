e5f08309a1f5177067ff6609a56134065d31a159
manashkhandelwal <manashkhandelwal171@gmail.com>
2024-01-29 16:00:09 +0530

    Delete products from HTML

--- javascript/checkout.js
+++ javascript/checkout.js
@@ -14,7 +14,7 @@ cart.forEach((cartItem)=> {
       }
   });
 cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -96,6 +96,9 @@ document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 document.querySelectorAll('.js-delete-link').forEach((link)=>{
   link.addEventListener('click',()=>{
     const productId = link.dataset.productId;
-    removeFromCart(productId); 
+    removeFromCart(productId);
+    
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   })
 })
