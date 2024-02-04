18439fa86f09226e13f7cda32f42b50c2bca3f03
manashkhandelwal <manashkhandelwal171@gmail.com>
2024-02-05 00:22:49 +0530

    Make payment summary interactive

--- javascript/checkout/orderSummary.js
+++ javascript/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
@@ -31,39 +32,39 @@ export function renderOrderSummary(){
         </div>
 
         <div class="cart-item-details-grid">
-          <img class="product-image"
-            src="${matchingProduct.image}">
+            <img class="product-image"
+                src="${matchingProduct.image}">
 
-          <div class="cart-item-details">
-            <div class="product-name">
-              ${matchingProduct.name}
+            <div class="cart-item-details">
+                <div class="product-name">
+                ${matchingProduct.name}
+                </div>
+                <div class="product-price">
+                ${formatCurrency(matchingProduct.priceCents)}
+                </div>
+                <div class="product-quantity">
+                <span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                </span>
+                <span class="update-quantity-link link-primary">
+                    Update
+                </span>
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                    Delete
+                </span>
+                </div>
             </div>
-            <div class="product-price">
-              ${formatCurrency(matchingProduct.priceCents)}
-            </div>
-            <div class="product-quantity">
-              <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-              </span>
-              <span class="update-quantity-link link-primary">
-                Update
-              </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                Delete
-              </span>
-            </div>
-          </div>
 
-          <div class="delivery-options">
-            <div class="delivery-options-title">
-              Choose a delivery option:
+            <div class="delivery-options">
+                <div class="delivery-options-title">
+                Choose a delivery option:
+                </div>
+                ${deliveryOptionsHTML(matchingProduct, cartItem)}
             </div>
-            ${deliveryOptionsHTML(matchingProduct, cartItem)}
-          </div>
+            </div>
         </div>
-      </div>
     `;
-  });
+    });
 
   function deliveryOptionsHTML(matchingProduct, cartItem){
     let html = '';
@@ -111,8 +112,11 @@ export function renderOrderSummary(){
       
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
-    })
-  })
+
+      renderPaymentSummary();
+    });
+  });
+  
   document.querySelectorAll('.js-delivery-option').forEach((element)=>{
     element.addEventListener('click',()=>{
       const {productId, deliveryOptionId} = element.dataset;
